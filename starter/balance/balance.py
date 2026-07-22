87b0423a48d9342ff90e2453a398b85017af48dd
Joseph Kormann <ajkormann@gmail.com>
2026-07-21 20:17:17 -0400

    All balance unit tests pass.

--- starter/balance/balance.py
+++ starter/balance/balance.py
@@ -2,20 +2,19 @@
 
 from transaction.transaction_category import TransactionCategory
 from transaction.transaction import Transaction
-from balance.balance_observer import IBalanceObserver
+#from balance.balance_observer import IBalanceObserver
+from balance_observer import IBalanceObserver
 
 class Balance:
     """Singleton to track the balance."""
 
     _instance = None
     _balance = 0.0
-    # _balance_observer = IBalanceObserver
     _balance_observer = []
 
     def __init__(self):
         """Initialize the balance. Prevent direct instantiation."""
         self._balance = 0.0
-        # pass
 
     @classmethod
     def get_instance(cls):
@@ -24,19 +23,16 @@ class Balance:
         return cls._instance
 
     def register_observer(self, balance_observer):
-        # _balance_observer = balance_observer
         self._balance_observer.append(balance_observer)
         pass
 
     def reset(self):
         """Reset the net balance to zero."""
         self._balance = 0.0
-        # pass
 
     def add_income(self, amount):
         """Add income to the balance."""
         self._balance = self._balance + amount
-        # pass
 
     def add_expense(self, amount):
         """Subtract expense from the balance."""
@@ -56,8 +52,8 @@ class Balance:
             self._balance = self._balance - transaction.amount
         else:
             raise ValueError("Do better")
-        self._balance_observer[0].update(self._balance, transaction)
-        # self._balance_observer.update(self._balance, transaction)
+        for oby in self._balance_observer:
+            oby.update(self._balance, transaction)
 
     def get_balance(self):
         """Get the current net balance."""
