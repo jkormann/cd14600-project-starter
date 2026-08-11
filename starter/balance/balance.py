86e3a3174330b14a17d804926c149a4486cfeade
Joseph Kormann <ajkormann@gmail.com>
2026-08-10 20:44:39 -0400

    Updated balance

--- starter/balance/balance.py
+++ starter/balance/balance.py
@@ -16,6 +16,13 @@ class Balance:
         """Initialize the balance. Prevent direct instantiation."""
         self._balance = 0.0
 
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._balance = 0.0
+            cls._instance._balance_observer = []
+        return cls._instance
+
     @classmethod
     def get_instance(cls):
         if cls._instance is None:
